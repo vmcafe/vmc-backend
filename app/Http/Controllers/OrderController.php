b1dbe3859db27efd964f98d0c38f051b03c90a03
sandidewod <52358402+sandidewod@users.noreply.github.com>
2021-05-12 20:00:36 +0700

    payment

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -59,16 +59,42 @@ class OrderController extends Controller
 
     public function getOrder()
     {
-        $user = auth()->user()->id;
-        $order = Order::where('orders.id_user', $user)
-            ->join('address', 'orders.id_address', '=', 'address.id')
-            ->join('products', 'orders.id_product', '=', 'products.id')
-            ->join('users', 'address.id_user', '=', 'users.id')
-            ->select('orders.*', 'address.phone', 'address.street', 'address.district',
-            'products.name', 'users.name')
-            ->get();
+        try {
+            $user = auth()->user()->id;
+            $order = Order::where('orders.id_user', $user)
+                ->join('address', 'orders.id_address', '=', 'address.id')
+                ->join('products', 'orders.id_product', '=', 'products.id')
+                ->join('users', 'address.id_user', '=', 'users.id')
+                ->select('orders.*', 'address.phone', 'address.street', 'address.district',
+                'products.name', 'users.name')
+                ->get();
+    
+    
+            return $this->responseSuccess($order);
+        } catch (\Throwable $e) {
+            return $this->responseException($e);
+        }
+    }
+    public function pembayaran(Request $request, $id)
+    {
+        $rules = [
+            'bank' => 'required',
+            'harga_total' => 'required|integer'
+        ];
+        $this->validate($request, $rules);
+        $order = Order::find($id);
 
-
-        return $order;
+        $order_id = $order->id;
+        $transaction_details = array(
+            'order_id'    => $order_id,
+            'gross_amount'  => $order->cost
+        );
+        $items = array(
+            array(
+                'price'    => $order->cost,
+                'quantity' => 1,
+                'name'     => 'Pembayaran booking wisata'
+            )
+        );
     }
 }
