762388fc03f8860a5ff8d795a03fc6637fda0d3b
sandidewod <52358402+sandidewod@users.noreply.github.com>
2021-06-21 02:04:38 +0700

    Update ProductController.php

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -30,7 +30,7 @@ class ProductController extends Controller
         $foto = $request->file('photo');
         $extension = $foto->getClientOriginalExtension();
         $foto->move('/home/vmct8135/public_html/uploads',$foto->getFilename().'.'.$extension);
-        $pathDefault = 'https://vmcafe.id/uploads/';
+        $pathDefault = '/home/vmct8135/public_html/uploads';
         $product->photo = $pathDefault.$foto->getFilename().'.'.$extension;
         $product->description = $request->description;
         $product->stock = $request->stock;
